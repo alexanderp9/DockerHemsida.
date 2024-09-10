b2c964238ca31d3e05d075a30eb27b5f90d63a8c
Alexa <alexander.pino@nackademin.yh.antagning.se>
2024-09-10 18:40:47 +0200

    bas

---
+++ src/main/java/com/example/devopshemsida/DevopsHemsidaApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopshemsida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsHemsidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsHemsidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopshemsida/DevopsHemsidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopshemsida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsHemsidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
