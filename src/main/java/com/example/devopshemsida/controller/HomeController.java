00f8041fcadc5f9f6dd5650a510b7fcdcc78d346
Alexa <alexander.pino@nackademin.yh.antagning.se>
2024-09-16 10:21:26 +0200

    bas

--- src/main/java/com/example/devopshemsida/controller/HomeController.java
+++ src/main/java/com/example/devopshemsida/controller/HomeController.java
@@ -12,10 +12,13 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
 
+        return "login";
 
+    }
 
-        return "index";
-
+    @GetMapping("/register")
+    public String register (Model model) {
+        return "register";
     }
 
 }
